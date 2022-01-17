10d35df2d1c701ccf75a88be5013e19b2d319f16
Nguyễn Chí Trung Kiên <41412023+ngchitrungkien@users.noreply.github.com>
2022-01-17 14:48:31 +0700

    Initial commit

---
+++ js/music-list.js
@@ -0,0 +1,63 @@
+let allMusic = [
+
+    {
+        name: "Ngày Qua Giông Bão",
+        artist: "Tùng Dương",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "Anywhere",
+        artist: "Ikson",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "Iceman",
+        artist: "Sol7 x MCK x DCOD",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "Bước Qua Nhau",
+        artist: "Vũ",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "Sun Goes Down",
+        artist: "Jim Yosef x Roy",
+        img: "music-5",
+        src: "music-5"
+    },
+    {
+        name: "Ái Nộ",
+        artist: "Masew x Khoi Vu",
+        img: "music-6",
+        src: "music-6"
+    },
+    {
+        name: "Sài Gòn Đau Lòng Quá",
+        artist: "Hứa Kim Tuyền x Hoàng Duyên",
+        img: "music-7",
+        src: "music-7"
+    },
+    {
+        name: "Cùng Anh",
+        artist: "Ngọc Dolil x Hagi x STee",
+        img: "music-8",
+        src: "music-8"
+    },
+    {
+        name: "Yêu Đừng Sợ Đau",
+        artist: "Ngô Lan Hương",
+        img: "music-9",
+        src: "music-9"
+    },
+    {
+        name: "I Love You 3000",
+        artist: "Stephanie Poetri",
+        img: "music-10",
+        src: "music-10"
+    },
+    ];
